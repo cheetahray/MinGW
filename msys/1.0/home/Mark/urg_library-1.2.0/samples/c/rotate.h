6887122dba99ad122cfa64f376318a4d39f6b81f
cheetahray <ray608@gmail.com>
2017-06-14 13:10:33 +0800

    Forgot header file

---
+++ msys/1.0/home/Mark/urg_library-1.2.0/samples/c/rotate.h
@@ -0,0 +1,14 @@
+#ifndef ROTATE_H
+#define ROTATE_H
+// kmeans.h
+// Ethan Brodsky
+// October 2011
+
+float inputMatrix[4][1];
+float outputMatrix[4][1];
+
+void showPoint();
+void multiplyMatrix();
+void setUpRotationMatrix(float angle, float u, float v, float w);
+
+#endif
