555279ddb856588e4423eb42a1ab842e82012ec0
ray <ray@dac.tw>
2017-09-01 21:25:03 +0800

    Change 100ms to 200ms move the sleep to only detect fingers

--- msys/1.0/home/Mark/urg_library-1.2.0/samples/c/calculate_xy_5.c
+++ msys/1.0/home/Mark/urg_library-1.2.0/samples/c/calculate_xy_5.c
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
     */
     lo_address t = lo_address_new("127.0.0.1", "12002");
     //urg_start_measurement(&urg, URG_DISTANCE, URG_SCAN_INFINITY, skip_scan);
-    int milisec = 100; // length of time to sleep, in miliseconds
+    int milisec = 200; // length of time to sleep, in miliseconds
     struct timespec req = {0};
     req.tv_sec = 0;
     req.tv_nsec = milisec * 1000000L;
@@ -102,7 +102,6 @@ int main(int argc, char *argv[])
         // Gets measurement data
         urg_start_measurement(&urg, URG_DISTANCE, scan_times, skip_scan);
         n = urg_get_distance(&urg, data, &time_stamp);
-        nanosleep(&req, (struct timespec *)NULL);
         if (n <= 0) {
             printf("urg_get_distance: %s\n", urg_error(&urg));
             urg_close(&urg);
@@ -169,7 +168,7 @@ int main(int argc, char *argv[])
 
             if ( lo_send(t, "/radar", "iii", 5, (int)( (outputMatrix[0][0]-Xmin) / unitX + 36.0 ), (int)( (Ymin+outputMatrix[1][0]) / -unitY + 42.0 ) ) == -1 )
                 printf("OSC error %d: %s\n", lo_address_errno(t), lo_address_errstr(t));
-
+            nanosleep(&req, (struct timespec *)NULL);
         }
 
         free(X);
