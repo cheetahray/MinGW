1c071a246873169a4dab32be0ef8af249891cfad
cheetahray <ray608@gmail.com>
2018-09-14 09:28:22 +0800

    Coordinate filled

--- msys/1.0/home/Mark/urg_library-1.2.0/samples/c/radar.py
+++ msys/1.0/home/Mark/urg_library-1.2.0/samples/c/radar.py
@@ -12,11 +12,11 @@ server = OSCServer( ("0.0.0.0", 12001) )
 server.timeout = 0.001
 run = True
 
-def click(pos, speed):
+def click(pos, speed, msg="/button"):
     global cc
     #unit = random.randint(5,5)
     oscmsg = OSC.OSCMessage()
-    oscmsg.setAddress("/button")
+    oscmsg.setAddress(msg)
     oscmsg.append(pos)
     oscmsg.append(speed)
     print oscmsg
@@ -44,51 +44,160 @@ def user_callback(path, tags, args, source):
     if args[0] == 3: 
         if mode[args[0]] == 0:
             if args[1] > -862.0 and args[1] < -544.0 and args[2] < -993.0 and args[2] > -1333.0:
-                click(3,0)
+                click(args[0],0)
             elif args[1] > -809.0 and args[1] < -538.0 and args[2] < -1682.0 and args[2] > -1922.0:
-                click(3,1)
+                click(args[0],1)
         elif mode[args[0]] == 1:
             if args[2] < -1038.0 and args[2] > -1257.0:
                 if args[1] > -1238.0 and args[1] < -985.0:
-                    click(3,0)
+                    click(args[0],0)
                 elif args[1] < -720.0:
-                    click(3,1)
+                    click(args[0],1)
                 elif args[1] < -472.0:
-                    click(3,2)
+                    click(args[0],2)
                 elif args[1] < -225.0:
-                    click(3,3)
+                    click(args[0],3)
         elif mode[args[0]] == 2:
             if args[2] < -1708.0 and args[2] > -1931.0:
                 if args[1] > -1261.0 and args[1] < -1001.0:
-                    click(3,0)
+                    click(args[0],0)
                 elif args[1] < -715.0:
-                    click(3,1)
+                    click(args[0],1)
                 elif args[1] < -472.0:
-                    click(3,2)
+                    click(args[0],2)
                 elif args[1] < -269.0:
-                    click(3,3)
-    '''
+                    click(args[0],3)
     elif args[0] == 4: 
         if mode[args[0]] == 0:
+            if args[1] > 594.0 and args[1] < 964.0 and args[2] < -1124.0 and args[2] > -1515.0:
+                click(args[0],0)
+            elif args[1] > 615.0 and args[1] < 894.0 and args[2] < -1805.0 and args[2] > -2055.0:
+                click(args[0],1)
         elif mode[args[0]] == 1:
+            if args[2] < -1231.0 and args[2] > -1440.0:
+                if args[1] > 293.0 and args[1] < 494.0:
+                    click(args[0],0)
+                elif args[1] < 750.0:
+                    click(args[0],1)
+                elif args[1] < 992.0:
+                    click(args[0],2)
+                elif args[1] < 1203.0:
+                    click(args[0],3)
         elif mode[args[0]] == 2:
+            if args[2] < -1881.0 and args[2] > -2089.0:
+                if args[1] > 286.0 and args[1] < 553.0:
+                    click(args[0],0)
+                elif args[1] < 759.0:
+                    click(args[0],1)
+                elif args[1] < 1021.0:
+                    click(args[0],2)
+                elif args[1] < 1223.0:
+                    click(args[0],3)
     elif args[0] == 5: 
         if mode[args[0]] == 0:
+            if args[1] > 631.0 and args[1] < 954.0 and args[2] < -2412.0 and args[2] > -2867.0:
+                click(args[0],0)
+            elif args[1] > 688.0 and args[1] < 1019.0 and args[2] < -3018.0 and args[2] > -3373.0:
+                click(args[0],1)
         elif mode[args[0]] == 1:
+            if args[2] < -2460.0 and args[2] > -2832.0:
+                if args[1] > 319.0 and args[1] < 560.0:
+                    click(args[0],0)
+                elif args[1] < 839.0:
+                    click(args[0],1)
+                elif args[1] < 1104.0:
+                    click(args[0],2)
+                elif args[1] < 1283.0:
+                    click(args[0],3)
         elif mode[args[0]] == 2:
+            if args[2] < -3104.0 and args[2] > -3399.0:
+                if args[1] > 328.0 and args[1] < 592.0:
+                    click(args[0],0)
+                elif args[1] < 838.0:
+                    click(args[0],1)
+                elif args[1] < 1097.0:
+                    click(args[0],2)
+                elif args[1] < 1324.0:
+                    click(args[0],3)
     elif args[0] == 6: 
         if mode[args[0]] == 0:
+            if args[1] > -991.0 and args[1] < -615.0 and args[2] < -2411.0 and args[2] > -2910.0:
+                click(args[0],0)
+            elif args[1] > -1012.0 and args[1] < -633.0 and args[2] < -3144.0 and args[2] > -3423.0:
+                click(args[0],1)
         elif mode[args[0]] == 1:
+            if args[2] < -2519.0 and args[2] > -2864.0:
+                if args[1] > -1263.0 and args[1] < -1092.0:
+                    click(args[0],0)
+                elif args[1] < -839.0:
+                    click(args[0],1)
+                elif args[1] < -528.0:
+                    click(args[0],2)
+                elif args[1] < -282.0:
+                    click(args[0],3)
         elif mode[args[0]] == 2:
+            if args[2] < -3213.0 and args[2] > -3421.0:
+                if args[1] > -1353.0 and args[1] < -1094.0:
+                    click(args[0],0)
+                elif args[1] < -839.0:
+                    click(args[0],1)
+                elif args[1] < -550.0:
+                    click(args[0],2)
+                elif args[1] < -312.0:
+                    click(args[0],3)
     elif args[0] == 7: 
         if mode[args[0]] == 0:
+            if args[1] > 653.0 and args[1] < 1036.0 and args[2] < -2377.0 and args[2] > -2933.0:
+                click(args[0],0)
+            elif args[1] > 639.0 and args[1] < 1017.0 and args[2] < -3101.0 and args[2] > -3428.0:
+                click(args[0],1)
         elif mode[args[0]] == 1:
+            if args[2] < -2454.0 and args[2] > -2919.0:
+                if args[1] > 342.0 and args[1] < 602.0:
+                    click(args[0],0)
+                elif args[1] < 860.0:
+                    click(args[0],1)
+                elif args[1] < 1103.0:
+                    click(args[0],2)
+                elif args[1] < 1371.0:
+                    click(args[0],3)
         elif mode[args[0]] == 2:
+            if args[2] < -3165.0 and args[2] > -3409.0:
+                if args[1] >  342.0 and args[1] < 560.0:
+                    click(args[0],0)
+                elif args[1] < 848.0:
+                    click(args[0],1)
+                elif args[1] < 1094.0:
+                    click(args[0],2)
+                elif args[1] < 1333.0:
+                    click(args[0],3)
     elif args[0] == 8: 
         if mode[args[0]] == 0:
+            if args[1] > -1074.0 and args[1] < -725.0 and args[2] < -2406.0 and args[2] > -2796.0:
+                click(args[0],0)
+            elif args[1] > -1027.0 and args[1] < -710.0 and args[2] < -3049.0 and args[2] > -3357.0:
+                click(args[0],1)
         elif mode[args[0]] == 1:
+            if args[2] < -2448.0 and args[2] > -2822.0:
+                if args[1] > -1382.0 and args[1] < -1145.0:
+                    click(args[0],0)
+                elif args[1] < -874.0:
+                    click(args[0],1)
+                elif args[1] < -615.0:
+                    click(args[0],2)
+                elif args[1] < -362.0:
+                    click(args[0],3)
         elif mode[args[0]] == 2:
-    '''
+            if args[2] < -3082.0 and args[2] > -3388.0:
+                if args[1] > -1434.0 and args[1] < -1150.0:
+                    click(args[0],0)
+                elif args[1] < -871.0:
+                    click(args[0],1)
+                elif args[1] < -601.0:
+                    click(args[0],2)
+                elif args[1] < -389.0:
+                    click(args[0],3)
+    
 def quit_callback(path, tags, args, source):
     # don't do this at home (or it'll quit blender)
     global run
@@ -98,6 +207,7 @@ def mode_callback(path, tags, args, source):
     # don't do this at home (or it'll quit blender)
     mode[args[0]] = args[1]
     #print args[0], args[1]
+    click(args[0], args[1], path)
     
 server.addMsgHandler( "/button1", user_callback )
 #server.addMsgHandler( "/quit", quit_callback )
